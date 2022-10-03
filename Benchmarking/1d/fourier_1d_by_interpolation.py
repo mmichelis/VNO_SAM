1a0e09c9300bbfe017483469687475ecbaf04603
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-10-03 11:48:46 +0200

    original data worked fine, although different bounds had higher error. going back to interpolated data

--- Benchmarking/1d/fourier_1d_by_interpolation.py
+++ Benchmarking/1d/fourier_1d_by_interpolation.py
@@ -177,8 +177,8 @@ loc -= torch.min(loc)
 end_id = torch.max(loc)
 
 # Data is of the shape (number of samples, grid size)
-# trainloader = MatReader('../../../VNO_data/1d/'+interp+'_from_'+data_dist+'_burgers_data_R10.mat')
-trainloader = MatReader('../../../VNO_data/1d/burgers_data_R10.mat')
+trainloader = MatReader('../../../VNO_data/1d/'+interp+'_from_'+data_dist+'_burgers_data_R10.mat')
+# trainloader = MatReader('../../../VNO_data/1d/burgers_data_R10.mat')
 x_data = trainloader.read_field('a')[:,:]
 y_data = trainloader.read_field('u')[:,:]
 
