84cd715e6fc8e97dbd462f995abe223d75397024
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-10-24 20:55:18 +0200

    plotting original data

--- Benchmarking/Earth/quick_plot.py
+++ Benchmarking/Earth/quick_plot.py
@@ -8,13 +8,22 @@ from utilities3 import *
 
 # reader = MatReader('./predictions/SPEED_data_100_ep5_m32_w40.mat')
 # prediction = reader.read_field('pred')
+
+# lon = np.arange(prediction.shape[1])
+# lat = np.arange(prediction.shape[2])
+
+# lat_, lon_ = np.meshgrid(lat, lon)
+
+# plt.contourf(lat_, lon_, prediction[0,:,:,0], 60, cmap='RdYlBu')
+# plt.show()
+
 reader = MatReader('../../../VNO_data/EarthData/SPEED_data_0.mat')
 prediction = reader.read_field('SPEED')
 
-lon = np.arange(prediction.shape[1])
+lon = np.arange(prediction.shape[3])
 lat = np.arange(prediction.shape[2])
 
 lat_, lon_ = np.meshgrid(lat, lon)
 
-plt.contourf(lat_, lon_, prediction[0,:,:,0], 60, cmap='RdYlBu')
+plt.contourf(lat_, lon_, prediction[0,0,:,:], 60, cmap='RdYlBu')
 plt.show()
