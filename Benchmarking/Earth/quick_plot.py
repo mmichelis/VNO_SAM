b474761a797a492f1e0ed7e5a103f52e616fd70e
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-10-24 21:29:02 +0200

    plot

--- Benchmarking/Earth/quick_plot.py
+++ Benchmarking/Earth/quick_plot.py
@@ -6,24 +6,24 @@ import matplotlib.pyplot as plt
 sys.path.append('../../')
 from utilities3 import *
 
-# reader = MatReader('./predictions/SPEED_data_100_ep5_m32_w40.mat')
-# prediction = reader.read_field('pred')
+reader = MatReader('./predictions/SPEED_data_100_ep5_m32_w40.mat')
+prediction = reader.read_field('pred')
 
-# lon = np.arange(prediction.shape[1])
-# lat = np.arange(prediction.shape[2])
-
-# lat_, lon_ = np.meshgrid(lat, lon)
-
-# plt.contourf(lat_, lon_, prediction[0,:,:,0], 60, cmap='RdYlBu')
-# plt.show()
-
-reader = MatReader('../../../VNO_data/EarthData/SPEED_data_0.mat')
-prediction = reader.read_field('SPEED')
-
-lon = np.arange(prediction.shape[3])
+lon = np.arange(prediction.shape[1])
 lat = np.arange(prediction.shape[2])
 
-lat_, lon_ = np.meshgrid(lat, lon)
+lon_, loat_ = np.meshgrid(lon, lat)
 
-plt.contourf(lat_, lon_, prediction[0,0,:,:], 60, cmap='RdYlBu')
+plt.contourf(lon_, lat_, prediction[0,:,:,0], 60, cmap='RdYlBu')
 plt.show()
+
+# reader = MatReader('../../../VNO_data/EarthData/SPEED_data_0.mat')
+# prediction = reader.read_field('SPEED')[:2,:2,::3,::3]
+
+# lon = np.arange(prediction.shape[3])
+# lat = np.arange(prediction.shape[2])
+
+# lon_, lat_ = np.meshgrid(lon, lat)
+
+# plt.contourf(lon_, lat_, prediction[0,0,:,:], 60, cmap='RdYlBu')
+# plt.show()
