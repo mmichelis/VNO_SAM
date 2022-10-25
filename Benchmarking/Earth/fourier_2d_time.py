df9498084ad8cf023d62f85cc0958f2bf9b5bc6f
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-10-25 15:57:51 +0200

    just looking at results from bias

--- Benchmarking/Earth/fourier_2d_time.py
+++ Benchmarking/Earth/fourier_2d_time.py
@@ -118,24 +118,33 @@ class FNO2d(nn.Module):
         x = x.permute(0, 3, 1, 2)
         # x = F.pad(x, [0,self.padding, 0,self.padding]) # pad the domain if input is non-periodic
 
-        x1 = self.conv0(x)
+        # x1 = self.conv0(x)
+        # x2 = self.w0(x)
+        # x = x1 + x2
+        # x = F.gelu(x)
+
+        # x1 = self.conv1(x)
+        # x2 = self.w1(x)
+        # x = x1 + x2
+        # x = F.gelu(x)
+
+        # x1 = self.conv2(x)
+        # x2 = self.w2(x)
+        # x = x1 + x2
+        # x = F.gelu(x)
+
+        # x1 = self.conv3(x)
+        # x2 = self.w3(x)
+        # x = x1 + x2
+
         x2 = self.w0(x)
-        x = x1 + x2
-        x = F.gelu(x)
-
-        x1 = self.conv1(x)
+        x = F.gelu(x2)
         x2 = self.w1(x)
-        x = x1 + x2
-        x = F.gelu(x)
-
-        x1 = self.conv2(x)
+        x = F.gelu(x2)
         x2 = self.w2(x)
-        x = x1 + x2
-        x = F.gelu(x)
-
-        x1 = self.conv3(x)
+        x = F.gelu(x2)
         x2 = self.w3(x)
-        x = x1 + x2
+        x = F.gelu(x2)
 
         # x = x[..., :-self.padding, :-self.padding] # pad the domain if input is non-periodic
         x = x.permute(0, 2, 3, 1)
@@ -160,7 +169,7 @@ ntrain = 100
 ntest = 100
 
 modes = 16
-width = 20
+width = 80
 
 batch_size = 2
 batch_size2 = batch_size
