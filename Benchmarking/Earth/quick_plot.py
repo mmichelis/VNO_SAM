8d737c818ab65de6cba4e4735d1a83319231c1e3
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-10-26 14:01:27 +0200

    fixing quickplot for gifs

--- Benchmarking/Earth/quick_plot.py
+++ Benchmarking/Earth/quick_plot.py
@@ -21,7 +21,8 @@ plt.show()
 
 
 # save an animation
-
+import pdb
+pdb.set_trace()
 fig = plt.figure()
 cont = plt.contourf(lat_, lon_, prediction[0,:,:,0],  60, cmap='RdYlBu')
 ax = plt.axes(xlim=(-180, 180), ylim=(-90, 90))
@@ -30,7 +31,7 @@ def update(frame_num):
     frame_num = int(frame_num)
     fig.clf()
     u = prediction[0,:,:,frame_num]
-    cont = plt.contourf(lon_, lat_, u, 60, cmap='RdYlBu')
+    cont = plt.contourf(lat_, lon_, u, 60, cmap='RdYlBu')
     plt.title('t=%i:' % frame_num)
     return cont
     
