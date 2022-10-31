9397735cad66caca40c1de67f22c03db8e5321b8
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-10-31 11:01:08 +0100

    chaning plot

--- Benchmarking/Earth/quick_plot.py
+++ Benchmarking/Earth/quick_plot.py
@@ -16,7 +16,7 @@ lat = np.arange(prediction.shape[2])
 
 lon_, lat_ = np.meshgrid(lat, lon)
 
-plt.contourf(lat_, lon_, prediction[0,:,:,0], 60, cmap='RdYlBu_r')
+plt.contourf(lat_, lon_, prediction[:,:,0], 60, cmap='RdYlBu_r')
 plt.show()
 
 
@@ -24,13 +24,13 @@ plt.show()
 import pdb
 # pdb.set_trace()
 fig = plt.figure()
-cont = plt.contourf(lat_, lon_, prediction[0,:,:,0],  60, cmap='RdYlBu_r')
+cont = plt.contourf(lat_, lon_, prediction[:,:,0],  60, cmap='RdYlBu_r')
 ax = plt.axes(xlim=(-180, 180), ylim=(-90, 90))
 
 def update(frame_num):
     frame_num = int(frame_num)
     fig.clf()
-    u = prediction[0,:,:,frame_num]
+    u = prediction[:,:,frame_num]
     cont = plt.contourf(lat_, lon_, u, 60, cmap='RdYlBu_r')
     plt.title('t=%i:' % frame_num)
     return cont
