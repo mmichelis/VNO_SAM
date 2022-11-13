b0542b3912cfafbab39986790998cfe28d69fc5d
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-11-13 21:33:31 +0100

    plot ground truth

--- VNO/2d/plotting/ground_truth.py
+++ VNO/2d/plotting/ground_truth.py
@@ -21,9 +21,10 @@ DATA_PATH_TRAIN = path_pred + pred_file
 pred_reader = MatReader(DATA_PATH_TRAIN)
 
 # create grid
-pos_x = pred_reader.read_field('x_pos')[0,:].numpy().astype(int)
-pos_y = pred_reader.read_field('y_pos')[0,:].numpy().astype(int)
-x, y = np.meshgrid(pos_x, pos_y)
+# pos_x = pred_reader.read_field('x_pos')[0,:].numpy().astype(int)
+# pos_y = pred_reader.read_field('y_pos')[0,:].numpy().astype(int)
+# x, y = np.meshgrid(pos_x, pos_y)
+
 
 # import the test data
 path_test = '../../../../VNO_data/2d/'
@@ -33,6 +34,7 @@ DATA_PATH_TEST = path_test + training_test
 # read the ground_truth data
 test_reader = MatReader(DATA_PATH_TEST)
 data = test_reader.read_field('vorticity')[0,...].numpy()
+x, y = np.mgrid[0:512,0:512]
 # data = np.take(np.take(data, pos_x, axis=0), pos_y, axis=1)
 
 # plot the prediction data
