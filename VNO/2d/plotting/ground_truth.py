29a5f836198ed56a3d94d025af05c88d32981148
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-11-13 21:05:50 +0100

    refining plotting

--- VNO/2d/plotting/ground_truth.py
+++ VNO/2d/plotting/ground_truth.py
@@ -19,7 +19,6 @@ pred_file = input('file name: ')
 
 DATA_PATH_TRAIN = path_pred + pred_file
 pred_reader = MatReader(DATA_PATH_TRAIN)
-data = pred_reader.read_field('pred')[:,:,:,:]
 
 # create grid
 pos_x = pred_reader.read_field('x_pos')
@@ -27,12 +26,13 @@ pos_y = pred_reader.read_field('y_pos')
 x, y = np.meshgrid(pos_x, pos_y)
 
 # import the test data
-# path_test = '../../../../VNO_data/2d/'
-# training_test =  'navierstokes_512_512_v1e-4_0.mat'
-# DATA_PATH_TEST = path_test + training_test
+path_test = '../../../../VNO_data/2d/'
+training_test =  'navierstokes_512_512_v1e-4_0.mat'
+DATA_PATH_TEST = path_test + training_test
 
-# # read using the utilities3 data reader
-# test_reader = MatReader(DATA_PATH_TEST)[:, pos_x, pos_y, :]
+# read the ground_truth data
+test_reader = MatReader(DATA_PATH_TEST)
+data = test_reader.read_field('vorticity')[:, pos_x, pos_y, -10:]
 
 # plot the prediction data
 s = 10
@@ -56,4 +56,4 @@ def init():
     ax = plt.axes(xlim=(0, 511), ylim=(0, 511))
     return cont
 ani = animation.FuncAnimation(fig, update, frames=s, interval = 100, init_func=init)
-ani.save('ani.gif', writer=animation.FFMpegWriter())
+ani.save('ground_truth.gif', writer=animation.FFMpegWriter())
