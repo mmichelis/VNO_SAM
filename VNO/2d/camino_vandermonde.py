756f8baa5bf8bb22b70dd2ea78a4f43be28172e7
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-11-15 14:27:18 +0100

    refining vno

--- VNO/2d/camino_vandermonde.py
+++ VNO/2d/camino_vandermonde.py
@@ -176,9 +176,9 @@ batch_size = 20
 batch_size2 = batch_size
 
 epochs = 500
-learning_rate = 0.001
-scheduler_step = 100
-scheduler_gamma = 0.5
+learning_rate = 0.005
+scheduler_step = 10
+scheduler_gamma = 0.97
 
 print(epochs, learning_rate, scheduler_step, scheduler_gamma)
 
