bfbbce47c81cbe53340f6268ac0cac1e7d75fae5
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-11-15 16:30:54 +0100

    rerunning some earth experiments

--- Benchmarking/earth/earth_2d_time.py
+++ Benchmarking/earth/earth_2d_time.py
@@ -1,9 +1,7 @@
 """
-This work was originally authored by Zongyi Li, to present the Fourier Neural Operator for 2D+time problem such as the Navier Stokes equation discussed in Section 5.3 in the [paper](https://arxiv.org/pdf/2010.08895.pdf).
-
-It has been modified by
-@author: Levi Lingsch
-to implement the 2D+time VNO on the Navier Stokes equation using data with a contracting-expanding distribution in both dimensions.
+@author: Zongyi Li
+This file is the Fourier Neural Operator for 2D problem such as the Navier-Stokes equation discussed in Section 5.3 in the [paper](https://arxiv.org/pdf/2010.08895.pdf),
+which uses a recurrent structure to propagates in time.
 """
 
 
@@ -13,6 +11,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import matplotlib.pyplot as plt
+import sys
 
 
 import operator
@@ -21,16 +20,14 @@ from functools import partial
 
 from timeit import default_timer
 
-import sys
 sys.path.append('../../')
-from vft import *
-from Adam import Adam
 from utilities3 import *
-import pdb
+from Adam import Adam
 
 torch.manual_seed(0)
 np.random.seed(0)
 
+import pdb
 
 ################################################################
 # fourier layer
@@ -60,14 +57,18 @@ class SpectralConv2d_fast(nn.Module):
 
     def forward(self, x):
         batchsize = x.shape[0]
+        #Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfft2(x)
 
-        x_ft = transformer.forward(x.cfloat())
-        
-        out_ft = torch.zeros(batchsize, self.out_channels,  self.modes1, self.modes2, dtype=torch.cfloat, device=x.device)
-        out_ft[:, :, :self.modes1, :self.modes2] = self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
-                
-        x = transformer.inverse(out_ft).real
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
 
+        #Return to physical space
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
         return x
 
 class FNO2d(nn.Module):
@@ -91,8 +92,8 @@ class FNO2d(nn.Module):
         self.modes2 = modes2
         self.width = width
         self.padding = 2 # pad the domain if input is non-periodic
-        self.fc0 = nn.Linear(T+2, self.width)
-        # input channel is 12: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        self.fc0 = nn.Linear(14, self.width)
+        # input channel is 14: the solution of the previous 12 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
 
         self.conv0 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
         self.conv1 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
@@ -136,6 +137,15 @@ class FNO2d(nn.Module):
         x2 = self.w3(x)
         x = x1 + x2
 
+        # x2 = self.w0(x)
+        # x = F.gelu(x2)
+        # x2 = self.w1(x)
+        # x = F.gelu(x2)
+        # x2 = self.w2(x)
+        # x = F.gelu(x2)
+        # x2 = self.w3(x)
+        # x = F.gelu(x2)
+
         # x = x[..., :-self.padding, :-self.padding] # pad the domain if input is non-periodic
         x = x.permute(0, 2, 3, 1)
         x = self.fc1(x)
@@ -144,28 +154,27 @@ class FNO2d(nn.Module):
         return x
 
     def get_grid(self, shape, device):
-        batchsize, size_x, size_y = shape[0], shape[2], shape[1]
-        # gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
-        gridx = lon
-        gridx = gridx.reshape(1, 1, size_x, 1).repeat([batchsize, size_y, 1, 1])
-        # gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
-        gridy = lat
-        gridy = gridy.reshape(1, size_y, 1, 1).repeat([batchsize, 1, size_x, 1])
+        batchsize, size_x, size_y = shape[0], shape[1], shape[2]
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
         return torch.cat((gridx, gridy), dim=-1).to(device)
 
 ################################################################
 # configs
 ################################################################
+
 ntrain = 100
 ntest = 100
 
-modes = 32
-width = 40
+modes = 16
+width = 20
 
-batch_size = 5
+batch_size = 1
 batch_size2 = batch_size
 
-epochs = 100
+epochs = 10
 learning_rate = 0.001
 scheduler_step = 100
 scheduler_gamma = 0.5
@@ -174,149 +183,85 @@ print(epochs, learning_rate, scheduler_step, scheduler_gamma)
 
 DAT = 'QLML'
 path = DAT+'_data_'+str(ntrain)+'_ep' + str(epochs) + '_m' + str(modes) + '_w' + str(width)
-
-runtime = np.zeros(2, )
+# path_model = '../VNO_models/'+path
+# path_train_err = 'results/'+path+'train.txt'
+# path_test_err = 'results/'+path+'test.txt'
+# path_image = 'image/'+path
 t1 = default_timer()
 
+sub = 1
 T_in = 12
 T = 12
 step = 1
 
-##############################################################
+################################################################
 # load data
 ################################################################
-# Due to the amount of data required for this project, it is necessary to construct the sparse data directly within this code. There is not enough storage elsewhere.
-def load_data():
-    TEST_PATH = f'../../../VNO_data/EarthData/{DAT}_data_0.mat'
-    reader = MatReader(TEST_PATH)
-    test_a = reader.read_field(DAT)[-ntest:,:T_in,:,:]
-    test_u = reader.read_field(DAT)[-ntest:,T_in:T+T_in,:,:]
-
-    TRAIN_PATH = f'../../../VNO_data/EarthData/{DAT}_data_1.mat'
-    reader = MatReader(TRAIN_PATH)
-    train_a = reader.read_field(DAT)[:ntrain,:T_in,:,:]
-    train_u = reader.read_field(DAT)[:ntrain,T_in:T+T_in,:,:]
-
-    for NUM in range(2, 5):
-        TRAIN_PATH = f'../../../VNO_data/EarthData/{DAT}_data_{NUM}.mat'
-        reader = MatReader(TRAIN_PATH)
-        train_a = torch.cat((train_a, reader.read_field(DAT)[:ntrain,:T_in,:,:]))
-        train_u = torch.cat((train_u, reader.read_field(DAT)[:ntrain,T_in:T+T_in,:,:]))
-
-    return test_a, test_u, train_a, train_u
-test_a, test_u, train_a, train_u = load_data()
-# shape at this point: [ntrain/ntest, 12, 361, 576]
-
-# the data must be centered longitudinally so that it stays on a lattice when doubled
-def center_longitutude(data, center):
-    lon_pts = data.shape[-1]
-    return torch.cat((data[:,:,:,center-lon_pts//2:], data[:,:,:,:center-lon_pts//2]), -1)
 center_lon = int(188 * 1.6)
-test_a = center_longitutude(test_a, center_lon)
-test_u = center_longitutude(test_u, center_lon)
-train_a = center_longitutude(train_a, center_lon)
-train_u = center_longitutude(train_u, center_lon)
-
-# define the lattice of points to select for the simulation
-def define_positions(center_lat, growth, offset):
-    # the bottom and left boundaries are both at 0, but not the top or right boundaries
-    top = 180 * 2
-    right = 360 * 1.6
-
-    # the data should already be centered longitudinally
-    center_lon = right//2
-
-    # define the bounds of the equispaced region
-    side_s = center_lat - offset
-    side_n = center_lat + offset
-    side_w = center_lon - offset
-    side_e = center_lon + offset
-
-    # calculate the number of points in each side of the nonequispaced region
-    num_s = np.floor(side_s**(1/growth))+1
-    num_n = np.floor((top - side_n)**(1/growth))+1
-    num_w = np.floor(side_w**(1/growth))
-    num_e = num_w #np.floor((right - side_e)**(1/growth))
-
-    # define the positions of points to each side
-    points_s = torch.flip(side_s - torch.round(torch.arange(num_s)**growth), [0])
-    points_n = side_n + torch.round(torch.arange(num_n)**growth)
-    points_w = torch.flip(side_w - torch.round(torch.arange(num_w)**growth),[0])
-    points_e = side_e + torch.round(torch.arange(num_e)**growth)
-
-    # print(f"east {num_e} west {num_w}")
-
-    # positions with equispaced distributions
-    central_lat = torch.arange(side_s+1, side_n)
-    central_lon = torch.arange(side_w+1, side_e)
-
-    # fix positions together
-    lat = torch.cat((points_s, central_lat, points_n))
-    lon = torch.cat((points_w, central_lon, points_e))
-    return lon.int(), lat.int()
 center_lat = 137 * 2
-growth = 1.5
-lon, lat = define_positions(center_lat, growth, 20)
+offset = 20
+left = center_lon - offset
+right = center_lon + offset
+bottom = center_lat - offset
+top = center_lat + offset
 
+TEST_PATH = f'../../../VNO_data/EarthData/{DAT}_data_0.mat'
+reader = MatReader(TEST_PATH)
+test_a = reader.read_field(DAT)[-ntest:,:T_in,::sub,::sub]
+test_u = reader.read_field(DAT)[-ntest:,T_in:T+T_in,::sub,::sub]
 
-# select the positions from the desired distribution and double accordingly
-def double_data(data, lon, lat):
-    sparse_data = torch.index_select(torch.index_select(data, -2, lat), -1, lon)
-    double_data = sparse_data
-    double_data = torch.cat((torch.flip(sparse_data, [-2,-1]), sparse_data), -2)
-    return double_data
-test_a = double_data(test_a, lon, lat)
-test_u = double_data(test_u, lon, lat)
-train_a = double_data(train_a, lon, lat)
-train_u = double_data(train_u, lon, lat)
-# shape at this point: [ntrain/ntest, 12, 194, 123]
+TRAIN_PATH = f'../../../VNO_data/EarthData/{DAT}_data_1.mat'
+reader = MatReader(TRAIN_PATH)
+train_a = reader.read_field(DAT)[:ntrain,:T_in,::sub,::sub]
+train_u = reader.read_field(DAT)[:ntrain,T_in:T+T_in,::sub,::sub]
 
-lon = lon * np.pi / 180 / 1.6
-lat = np.pi - lat * np.pi / 180 / 2
-lat = torch.cat((torch.flipud(lat), 2*np.pi - lat), 0)
-# lat_, lon_ = torch.meshgrid(lat, lon)
-# plt.contourf(lon_, lat_, test_a[0,0,:,:], 60, cmap='RdYlBu_r')
-# plt.scatter(lon_, lat_, marker='.')
-# plt.show()
+for NUM in range(2, 5):
+    TRAIN_PATH = f'../../../VNO_data/EarthData/{DAT}_data_{NUM}.mat'
+    reader = MatReader(TRAIN_PATH)
+    train_a = torch.cat((train_a, reader.read_field(DAT)[:ntrain,:T_in,::sub,::sub]))
+    train_u = torch.cat((train_u, reader.read_field(DAT)[:ntrain,T_in:T+T_in,::sub,::sub]))
 
 # I am concatenating several large data file together here, so the ntrain is variable. Should just reset it here with the actual value.
 ntrain = train_a.shape[0]
+
 print(train_u.shape)
 print(test_u.shape)
 
 # can't assert without knowing shapes beforehand, so I just gather them from the data and use them where necessary
 S_x = train_u.shape[-1]
 S_y = train_u.shape[-2]
+lon = np.arange(S_x)
+lat = np.arange(S_y)
+
+lon_, lat_ = np.meshgrid(lat, lon)
+
 assert (T == train_u.shape[1])
 
-# a_normalizer = UnitGaussianNormalizer(train_a)
-# train_a = a_normalizer.encode(train_a)
-# test_a = a_normalizer.encode(test_a)
-# pdb.set_trace()
-
-# reshape the data to be in [Number of exmaples, X-coordinates, Y-coordinates, 1, Time], this is how their code was originally written
-train_a = torch.swapaxes(torch.swapaxes(train_a, 1, 3), 1, 2)
-train_u = torch.swapaxes(torch.swapaxes(train_u, 1, 3), 1, 2)
-test_a = torch.swapaxes(torch.swapaxes(test_a, 1, 3), 1, 2)
-test_u = torch.swapaxes(torch.swapaxes(test_u, 1, 3), 1, 2)
-# shape at this point: [ntrain/ntest, 123, 194, 12]
-
+# NOTE: using reshape will severely alter the data. Use torch. swapaxes instead to get it into the correct format.
+# go from (0, 1, 2, 3) to (0, 3, 2, 1)
+# train_a = train_a.reshape(ntrain,S_x,S_y,T_in)
+# test_a = test_a.reshape(ntest,S_x,S_y,T_in)
+train_a = torch.swapaxes(train_a, 1, 3)
+test_a = torch.swapaxes(test_a, 1, 3)
+train_u = torch.swapaxes(train_u, 1, 3)
+test_u = torch.swapaxes(test_u, 1, 3)
 
 train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_a, train_u), batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(test_a, test_u), batch_size=batch_size, shuffle=False)
 
 t2 = default_timer()
-
 print('preprocessing finished, time used:', t2-t1)
 device = torch.device('cuda')
-
-
+# pdb.set_trace()
+# plt.contourf(lat_, lon_, train_a[100,:,:,0].cpu().numpy(), cmap='RdYlBu')
+# plt.show()
 ################################################################
 # training and evaluation
 ################################################################
 
 model = FNO2d(modes, modes, width).cuda()
-transformer = vft2d(lon, lat, modes, modes)
+# model = torch.load(path_model)
+
 
 print(count_params(model))
 optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
@@ -324,8 +269,8 @@ scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step,
 
 myloss = LpLoss(size_average=False)
 
-training_history = open('./training_history/2d_vandermonde.txt', 'w')
-training_history.write('Epoch  Time  Train_L2_Step  Train_L2_Full  Test_L2_Step  Test_L2_Full  \n')
+training_history = open(f'./training_history/2d_earth_{DAT}_data.txt', 'w')
+training_history.write('Epoch  Time  Train_L2_step Train_L2_full Test_L2_step Test_L2_full \n')
 
 for ep in range(epochs):
     model.train()
@@ -335,13 +280,13 @@ for ep in range(epochs):
     for xx, yy in train_loader:
         loss = 0
         xx = xx.to(device)
-        yy = yy.to(device)
+        yy = yy.to(device)[:,left:right, bottom:top, :]
 
         for t in range(0, T, step):
-            # pdb.set_trace()
+
             y = yy[..., t:t + step]
 
-            im = model(xx)
+            im = model(xx)[:,left:right, bottom:top,:]
 
             loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
 
@@ -366,13 +311,13 @@ for ep in range(epochs):
         for xx, yy in test_loader:
             loss = 0
             xx = xx.to(device)
-            yy = yy.to(device)
+            yy = yy.to(device)[:,left:right, bottom:top, :]
 
 
             for t in range(0, T, step):
                 y = yy[..., t:t + step]
 
-                im = model(xx)
+                im = model(xx)[:,left:right, bottom:top,:]
                 
                 loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
 
@@ -402,6 +347,7 @@ for ep in range(epochs):
     # plt.contourf(lat_, lon_, yy[0,:,:,0].cpu().numpy(), 60, cmap='RdYlBu')
     # plt.show()
 training_history.close()
+# torch.save(model, path_model)
 
 
 
@@ -409,19 +355,21 @@ training_history.close()
 # pred = torch.zeros(test_u.shape)
 index = 0
 test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(test_a, test_u), batch_size=1, shuffle=False)
-prediction_history = open('./training_history/2d_vandermonde_test_loss.txt', 'w')
-# ll: adding this to put y_norm on cuda
-# y_normalizer.cuda()
+prediction_history = open(f'./training_history/2d_earth_{DAT}_data_test_loss.txt', 'w')
+batch_size=1        # need to set this otherwise the loss outputs are not correct
+# full_pred = torch.zeros(test_u.shape)
 with torch.no_grad():
     for xx, yy in test_loader:
-        loss = 0
+        step_loss = 0
         xx = xx.to(device)
-        yy = yy.to(device)
+        yy = yy.to(device)[:,left:right, bottom:top, :]
         
-        # full_pred = model(xx)
         for t in range(0, T, step):
             y = yy[..., t:t + step]
-            im = model(xx)
+
+            im = model(xx)[:,left:right, bottom:top,:]
+
+            step_loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
 
             if t == 0:
                 pred = im
@@ -429,17 +377,13 @@ with torch.no_grad():
                 pred = torch.cat((pred, im), -1)
 
             xx = torch.cat((xx[..., step:], im), dim=-1)
-
-        loss = myloss(pred.reshape(1, -1), yy.reshape(1, -1)).item()
-
-        print(index, loss)
-        index = index + 1
-
-        # full_pred = torch.cat((full_pred, pred), -1)
         
-        prediction_history.write(str(loss / T)+'\n')
+        full_loss = myloss(pred.reshape(1, -1), yy.reshape(1, -1)).item()
+
+        print(index, full_loss.item() / T, step_loss.item() / T)
+        index = index + 1
+        prediction_history.write(f'{full_loss.item() / T}   {step_loss.item() / T}')
 prediction_history.close()
+print(pred.shape)
 
-
-# only save one prediction to keep space low
-scipy.io.savemat('./predictions/'+path+'.mat', mdict={'pred': pred.cpu().numpy(), 'lat': lat.cpu().numpy(), 'lon': lon.cpu().numpy()})
+scipy.io.savemat('./predictions/2d_earth_'+path+'.mat', mdict={'pred': pred.cpu().numpy()})
