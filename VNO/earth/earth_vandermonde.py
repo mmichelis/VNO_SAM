fe77a1310b4abacc0cd3b4d360de735d72bfb69e
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-11-21 14:24:07 +0100

    using both weights for earth vandermonde

--- VNO/earth/earth_vandermonde.py
+++ VNO/earth/earth_vandermonde.py
@@ -63,7 +63,7 @@ class SpectralConv2d_fast(nn.Module):
 
         x_ft = transformer.forward(x.cfloat())
         
-        out_ft = torch.zeros(batchsize, self.out_channels,  self.modes1, self.modes2, dtype=torch.cfloat, device=x.device)
+        out_ft = torch.zeros(batchsize, self.out_channels,  2 * self.modes1, self.modes2, dtype=torch.cfloat, device=x.device)
         out_ft[:, :, :self.modes1, :self.modes2] = self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
         out_ft[:, :, -self.modes1:, :self.modes2] = self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
                 
