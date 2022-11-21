2e7115a7742ffdea1abda28614ee492e99af219a
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-11-21 15:51:25 +0100

    going to try a longer run for earth

--- VNO/earth/earth_vandermonde.py
+++ VNO/earth/earth_vandermonde.py
@@ -166,7 +166,7 @@ class FNO2d(nn.Module):
 # configs
 ################################################################
 
-selected_modes = np.concatenate((np.arange(16)), np.arange(16,50,3))
+selected_modes = np.concatenate((np.arange(16), np.arange(16,50,3)))
 # selected_modes = np.arange(16)
 print(f'selected modes: {selected_modes}')
 modes = selected_modes.shape[0]
