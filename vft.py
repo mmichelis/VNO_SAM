57083e0235653a408950d6204bffb5b3471423b7
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-11-21 14:40:14 +0100

    testing out some stuff with earth

--- vft.py
+++ vft.py
@@ -108,9 +108,10 @@ class vdfs:
         V_y = torch.zeros([2*self.y_m, self.y_l], dtype=torch.cfloat).cuda()
         for row in range(self.y_m):
              for col in range(self.y_l):
-                # V_y[row, col] = np.exp(-1j * self.y_modes[row] *  self.y_positions[col]) 
-                V_y[row, col] = np.sin((2*self.y_modes[row]+1) *  self.y_positions[col] / 2) 
-                V_y[-row, col] = np.sin((2*(self.y_l - self.y_modes[row]+1)) *  self.y_positions[col] / 2) 
+                V_y[row, col] = np.exp(-1j * self.y_modes[row] *  self.y_positions[col]) 
+                V_y[-row, col] = np.exp(-1j * (self.y_l - self.y_modes[row]) *  self.y_positions[col]) 
+                # V_y[row, col] = np.sin((2*self.y_modes[row]) *  self.y_positions[col] / 2) 
+                # V_y[-row, col] = np.sin((2*(self.y_l - self.y_modes[row])) *  self.y_positions[col] / 2) 
         V_y = torch.divide(V_y, np.sqrt(self.y_l))
 
         return torch.transpose(V_x, 0, 1), torch.conj(V_x), torch.transpose(V_y, 0, 1), torch.conj(V_y)
