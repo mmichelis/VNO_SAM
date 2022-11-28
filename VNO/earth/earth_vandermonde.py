e0a1b59ae1bb5bf8dca3d9583fbcef5b73178a04
LeviLingsch <levi.lingsch@srl.ethz.ch>
2022-11-28 11:15:05 +0100

    use right basis

--- VNO/earth/earth_vandermonde.py
+++ VNO/earth/earth_vandermonde.py
@@ -63,13 +63,13 @@ class SpectralConv2d_fast(nn.Module):
     def forward(self, x):
         # batchsize = x.shape[0]
 
-        x_ft = transformer.forward(x.cfloat())
+        x_ft = transformer.forward(x)
         # Multiply relevant Fourier modes
         # out_ft = torch.zeros(batchsize, self.out_channels,  2 * self.modes1, self.modes2, dtype=torch.cfloat, device=x.device)
         x_ft[:, :, :self.modes1, :self.modes2] = self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
         # x_ft[:, :, -self.modes1:, :self.modes2] = self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
         #Return to physical space
-        x = transformer.inverse(x_ft).real
+        x = transformer.inverse(x_ft)
 
 
         # x_ft = transformer.forward(x.cfloat())
