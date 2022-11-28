fixing normalization for europe 2d time

Benchmarking/earth/europe_2d_time.py
@@ -217,6 +217,14 @@ def load_data():
     return test_a, test_u, train_a, train_u
 test_a, test_u, train_a, train_u = load_data()
 
+
+a_normalizer = RangeNormalizer(train_a)
+train_a = a_normalizer.encode(train_a)
+test_a = a_normalizer.encode(test_a)
+
+y_normalizer = RangeNormalizer(train_u)
+train_u = y_normalizer.encode(train_u)
+
 # I am concatenating several large data file together here, so the ntrain is variable. Should just reset it here with the actual value.
 ntrain = train_a.shape[0]
 ntest = test_a.shape[0]
@@ -269,6 +277,7 @@ myloss = LpLoss(size_average=False)
 training_history = open(f'./training_history/2d_europe_{DAT}_data.txt', 'w')
 training_history.write('Epoch  Time  Train_L2_step Train_L2_full Test_L2_step Test_L2_full \n')
 
+y_normalizer.cuda()
 for ep in range(epochs):
     model.train()
     t1 = default_timer()
@@ -314,7 +323,7 @@ for ep in range(epochs):
                 y = yy[..., t:t + step]
 
                 im = model(xx)
-                
+                im = y_normalizer.decode(im)
                 loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
 
                 if t == 0:
@@ -359,7 +368,7 @@ with torch.no_grad():
             y = yy[..., t:t + step]
 
             im = model(xx)
-
+            im = y_normalizer.decode(im)
             step_loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
 
             if t == 0:

VNO/earth/earth_vandermonde.py
@@ -460,7 +460,7 @@ with torch.no_grad():
             
             if t == 0:
                 pred = im
-                full_pred = full_im
+                full_pred = y_normalizer.decode(full_im)
             else:
                 pred = torch.cat((pred, im), -1)
                 full_pred = torch.cat((full_pred, y_normalizer.decode(full_im)), -1)